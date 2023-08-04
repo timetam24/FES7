e1374919f68ecc6c7b38412ebbaccf136e5015a6
timetam <timetam24@naver.com>
2023-08-04 15:14:12 +0900

    구현실습_로그인화면 - JS 추가

--- html&css/2023-07-25/challenge/app.js
+++ html&css/2023-07-25/challenge/app.js
@@ -1,26 +1,60 @@
 const loginBtn = document.querySelector(".login-btn");
 
-const loginBtnHandler = () => {
-  const userId = document.querySelector(".user-id");
-  const userPwd = document.querySelector(".user-password");
+const userId = document.querySelector(".user-id");
+const userPwd = document.querySelector(".user-pwd");
+const firstErrorMsg = document.createElement("p");
+const secondErrorMsg = document.createElement("p");
 
-  const idErrorMsg = document.querySelector(".id-error");
-  const pwdErrorMsg = document.querySelector(".pwd-error");
+const loginBtnHandler = (event) => {
+  event.preventDefault();
 
-  if (userId.value.trim() === "") {
+  const createErrorMsg = (element, className, text, nodeBehindErrorMsg) => {
+    element.classList.add(className);
+    element.innerText = text;
+    userPwd.parentNode.insertBefore(element, nodeBehindErrorMsg);
+  };
+
+  const removeErrorMsg = (errorMsg) => {
+    if (errorMsg.parentNode) {
+      errorMsg.parentNode.removeChild(errorMsg);
+    } else {
+      return;
+    }
+  };
+
+  if (!userId.value.trim()) {
+    // 아이디 미입력 시
     userId.classList.add("focus");
-    idErrorMsg.classList.remove("hidden");
-  } else {
+    createErrorMsg(
+      firstErrorMsg,
+      "first-error-msg",
+      "아이디를 입력해주세요.",
+      userPwd
+    );
+  } else if (userId.value.trim()) {
+    // 아이디 입력 시
     userId.classList.remove("focus");
-    idErrorMsg.classList.add("hidden");
-  }
+    removeErrorMsg(firstErrorMsg);
 
-  if (userPwd.value.trim() === "" && userId.value.trim() !== "") {
-    userPwd.classList.add("focus");
-    pwdErrorMsg.classList.remove("hidden");
-  } else {
-    userPwd.classList.remove("focus");
-    pwdErrorMsg.classList.add("hidden");
+    if (!userPwd.value.trim()) {
+      // 아이디 입력 & 비밀번호 미입력 시
+      userPwd.classList.add("focus");
+      createErrorMsg(
+        secondErrorMsg,
+        "second-error-msg",
+        "비밀번호를 입력해주세요.",
+        userPwd.nextSibling
+      );
+    } else if (userPwd.value.trim()) {
+      // 아이디 입력 & 비밀번호 입력 시
+      userPwd.classList.remove("focus");
+      if (userId.value !== "weniv07" && userPwd.value !== "frontend07!") {
+        secondErrorMsg.innerText = "아이디 혹은 비밀번호가 일치하지 않습니다.";
+      } else {
+        removeErrorMsg(secondErrorMsg);
+        alert("로그인 성공!");
+      }
+    }
   }
 };
 
